56495f997d0ea782b2135d8ededa1ebdf96fb9ac
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-11-27 12:45:47 -0300

    fix - adicionando logica das estrelas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import * as S from './Appp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <S.Star>
+    <div class="page-container"> 
+    oi
+      <div id='stars'></div>
+      <div id='stars2'></div>
+      <div id='stars3'></div>
     </div>
+    </S.Star>
   );
 }
 
